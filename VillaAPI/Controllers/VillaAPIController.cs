4427829b652ba8bdd41b0ded76f5af34f280dd01
Your Name <hertyv72@gmail.com>
2024-03-08 01:03:53 +0700

    Create the first endpoint

---
+++ VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VillaAPI
+{
+    [ApiController]
+    [Route("api/VillaAPI")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+                new Villa {Id = 1, Name = "Pool View"},
+                new Villa {Id = 2, Name = "Beach View"},
+            };
+        }
+    }
+}
---
+++ VillaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VillaAPI
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
