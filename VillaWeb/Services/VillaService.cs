1978bb0180216695c6fd5d1d57270ce028ef2609
Nguyen Dinh Anh Tien <hertyv72@gmail.com>
2024-06-08 23:43:36 +0700

    Add service for calling villa api

---
+++ VillaWeb/Services/VillaService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using VillaUtility;
+using VillaWeb.Models;
+using VillaWeb.Models.Dto;
+using VillaWeb.Services.IServices;
+
+namespace VillaWeb.Services;
+
+public class VillaService : BaseService, IVillaService
+{
+    protected readonly string _villaUrl;
+    public VillaService(IHttpClientFactory httpClientFactory, IConfiguration configuration) : base(httpClientFactory)
+    {
+        _villaUrl = configuration["ServiceUrls:VillaAPI"];
+    }
+
+    public Task<T> CreateAsync<T>(VillaCreateDTO dto)
+    {
+        return SendAsync<T>(new APIRequest() {
+            ApiType = SD.ApiType.POST,
+            Url = _villaUrl + "/api/VillaAPI",
+            Data = dto,
+        });
+    }
+
+    public Task<T> DeleteAsync<T>(int id)
+    {
+        return SendAsync<T>(new APIRequest() {
+            ApiType = SD.ApiType.DELETE,
+            Url = _villaUrl + "/api/VillaAPI/" + id,
+        });
+    }
+
+    public Task<T> GetAllAsync<T>()
+    {
+        return SendAsync<T>(new APIRequest() {
+            ApiType = SD.ApiType.GET,
+            Url = _villaUrl + "/api/VillaAPI",
+        });
+    }
+
+    public Task<T> GetAsync<T>(int id)
+    {
+        return SendAsync<T>(new APIRequest() {
+            ApiType = SD.ApiType.GET,
+            Url = _villaUrl + "/api/VillaAPI/" + id,
+        });
+    }
+
+    public Task<T> UpdateAsync<T>(int id, VillaUpdateDTO dto)
+    {
+        return SendAsync<T>(new APIRequest() {
+            ApiType = SD.ApiType.PUT,
+            Url = _villaUrl + "/api/VillaAPI/" + id,
+            Data = dto,
+        });    
+    }
+}
