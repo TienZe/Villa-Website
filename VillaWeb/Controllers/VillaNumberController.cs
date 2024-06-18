f6b535c0db908aeaec154660afa856c4ad8a29d4
TienZe <hertyv72@gmail.com>
2024-06-18 13:10:23 +0700

    Create get all VillaNumber view

---
+++ VillaWeb/Controllers/VillaNumberController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using VillaWeb.Models;
+using VillaWeb.Models.Dto;
+using VillaWeb.Services.IServices;
+
+namespace VillaWeb;
+public class VillaNumberController : Controller
+{
+    private readonly IMapper _mapper;
+    private readonly IVillaNumberService _villaNumberService;
+
+    public VillaNumberController(IMapper mapper, IVillaNumberService villaNumberService)
+    {
+        _mapper = mapper;
+        _villaNumberService = villaNumberService;
+    }
+
+    public async Task<IActionResult> IndexVillaNumber()
+    {
+        List<VillaNumberDTO> list = new();
+        var apiResponse = await _villaNumberService.GetAllAsync<APIResponse>();
+
+        if (apiResponse is not null && apiResponse.IsSuccess) {
+            list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(apiResponse.Result));
+        }
+        return View(list);
+    }
+
+}
--- VillaWeb/Models/Dto/VillaNumberDTO.cs
+++ VillaWeb/Models/Dto/VillaNumberDTO.cs
@@ -8,4 +8,5 @@ public class VillaNumberDTO
     public int VillaNo { get; set; }
     public string SpecialDetails { get; set; }
     public int VillaID { get; set; }
+    public VillaDTO Villa { get; set; }
 }
