a6f28c12d1b9b12891ef3267b2fc708ee2b792a9
TienZe <hertyv72@gmail.com>
2024-06-30 10:32:42 +0700

    Add villa's image local path column

---
+++ VillaAPI/Migrations/20240629102811_AddVillaImageLocalPath.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VillaAPI.Migrations
+{
+    public partial class AddVillaImageLocalPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7536ec76-5a36-402e-9a64-8517f6acce46");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fee2429e-d694-467c-9183-6c2a127d9a99");
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLocalPath",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageLocalPath",
+                table: "Villas");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "7536ec76-5a36-402e-9a64-8517f6acce46", "b81c2e88-1c2c-4544-b686-cc47a2d40abd", "Admin", "ADMIN" },
+                    { "fee2429e-d694-467c-9183-6c2a127d9a99", "4f9b24e1-bd37-4cb5-8598-3f928ac0bfad", "User", "USER" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenity", "CreatedDate", "Details", "ImageUrl", "Name", "Occupancy", "Rate", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(2024, 6, 28, 1, 17, 31, 373, DateTimeKind.Local).AddTicks(8476), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "(https://dotnetmastery.com/bluevillaimages/villa3.jpg", "Royal Villa", 4, 200.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "", new DateTime(2024, 6, 28, 1, 17, 31, 373, DateTimeKind.Local).AddTicks(8488), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://dotnetmastery.com/bluevillaimages/villa1.jpg", "Premium Pool Villa", 4, 300.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "", new DateTime(2024, 6, 28, 1, 17, 31, 373, DateTimeKind.Local).AddTicks(8489), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://dotnetmastery.com/bluevillaimages/villa4.jpg", "Luxury Pool Villa", 4, 400.0, 750, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, "", new DateTime(2024, 6, 28, 1, 17, 31, 373, DateTimeKind.Local).AddTicks(8491), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://dotnetmastery.com/bluevillaimages/villa5.jpg", "Diamond Villa", 4, 550.0, 900, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, "", new DateTime(2024, 6, 28, 1, 17, 31, 373, DateTimeKind.Local).AddTicks(8492), "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://dotnetmastery.com/bluevillaimages/villa2.jpg", "Diamond Pool Villa", 4, 600.0, 1100, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+    }
+}
--- VillaAPI/Models/Villa.cs
+++ VillaAPI/Models/Villa.cs
@@ -10,12 +10,13 @@ public class Villa
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string? Details { get; set; } = string.Empty;
+    public string? Details { get; set; }
     public double Rate { get; set; }
     public int Sqft { get; set; }
     public int Occupancy { get; set; }
-    public string? ImageUrl { get; set; } = string.Empty;
-    public string? Amenity { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public string? ImageLocalPath { get; set; }
+    public string? Amenity { get; set; }
     public DateTime UpdatedDate { get; set; }
     public DateTime CreatedDate { get; set; }
 }
