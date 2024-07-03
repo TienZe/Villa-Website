73cb48439aeefabd1e55a28850f2102542febf83
TienZe <hertyv72@gmail.com>
2024-07-03 10:35:45 +0700

    Refactor BaseService to encapsulate the http message initialization

--- VillaWeb/Services/BaseService.cs
+++ VillaWeb/Services/BaseService.cs
@@ -22,59 +22,64 @@ public class BaseService : IBaseService
     {
         try {
             var client = HttpClient.CreateClient("VillaAPI");
-            HttpRequestMessage message = new() {
-                RequestUri = new Uri(apiRequest.Url)
+
+            var messageFactory = () => {
+                HttpRequestMessage message = new() {
+                    RequestUri = new Uri(apiRequest.Url)
+                };
+
+                var tokenDTO = TokenProvider.GetToken();
+                if (tokenDTO is not null) {
+                    message.Headers.Add("Authorization", "Bearer " + tokenDTO.AccessToken);
+                }
+                    
+                if (apiRequest.Data is not null) {
+                    // Has data, need to serilize this data to the body of the request
+
+                    if (apiRequest.ContentType == ContentTypes.MultipartFormData) {
+                        var formData = new MultipartFormDataContent();
+
+                        foreach (var prop in apiRequest.Data.GetType().GetProperties()) {
+                            var propValue = prop.GetValue(apiRequest.Data);
+
+                            if (propValue is null) continue;
+
+                            if (propValue is IFormFile file) {
+                                formData.Add(new StreamContent(file.OpenReadStream()), prop.Name, file.FileName);
+                            } else {
+                                formData.Add(new StringContent(propValue.ToString()!), prop.Name);
+                            }
+                        }
+
+                        message.Content = formData;
+                    } else {
+                        message.Content = new StringContent(
+                            JsonConvert.SerializeObject(apiRequest.Data), 
+                            Encoding.UTF8, "application/json"
+                        );
+                    }
+                    
+                }
+
+                switch (apiRequest.ApiType) {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                return message;
             };
 
-            var tokenDTO = TokenProvider.GetToken();
-            if (tokenDTO is not null) {
-                message.Headers.Add("Authorization", "Bearer " + tokenDTO.AccessToken);
-            }
-                
-            if (apiRequest.Data is not null) {
-                // Has data, need to serilize this data to the body of the request
-
-                if (apiRequest.ContentType == ContentTypes.MultipartFormData) {
-                    var formData = new MultipartFormDataContent();
-
-                    foreach (var prop in apiRequest.Data.GetType().GetProperties()) {
-                        var propValue = prop.GetValue(apiRequest.Data);
-
-                        if (propValue is null) continue;
-
-                        if (propValue is IFormFile file) {
-                            formData.Add(new StreamContent(file.OpenReadStream()), prop.Name, file.FileName);
-                        } else {
-                            formData.Add(new StringContent(propValue.ToString()!), prop.Name);
-                        }
-                    }
-
-                    message.Content = formData;
-                } else {
-                    message.Content = new StringContent(
-                        JsonConvert.SerializeObject(apiRequest.Data), 
-                        Encoding.UTF8, "application/json"
-                    );
-                }
-                
-            }
-
-            switch (apiRequest.ApiType) {
-                case SD.ApiType.POST:
-                    message.Method = HttpMethod.Post;
-                    break;
-                case SD.ApiType.PUT:
-                    message.Method = HttpMethod.Put;
-                    break;
-                case SD.ApiType.DELETE:
-                    message.Method = HttpMethod.Delete;
-                    break;
-                default:
-                    message.Method = HttpMethod.Get;
-                    break;
-            }
-
-            HttpResponseMessage apiResponse = await client.SendAsync(message);
+            HttpResponseMessage apiResponse = await client.SendAsync(messageFactory());
             
             string apiContent = await apiResponse.Content.ReadAsStringAsync();
             var apiResponseObject = JsonConvert.DeserializeObject<T>(apiContent);
